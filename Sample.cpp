47276216c71b494782e2f3bfc61241c8c11f8481
Muhammad Ismail Soboute <misoboute@gmail.com>
2017-06-20 15:40:11 +0430

    Demonstrating usage of parameterized test steps in the Sample.cpp

--- Sample.cpp
+++ Sample.cpp
@@ -151,23 +151,24 @@ private:
 
 struct CalcTestContext {
     FakeCaclUserInterface* UI;
-    MyCalcApplication* App;
+    std::shared_ptr<MyCalcApplication> App;
 };
 
 // Each step of the test scenario inherits AccTestStep<T> where T is the type serving as our text context.
 // Apart from that, every step class will probably need to override the Act() and Verify() methods. Some test steps might also
 // need to override Setup(), Expect(), and Teardown(). See AccTestStep for more info.
 
-class TestStep1InitApp : public AccTestStep<CalcTestContext> {
+class TestStepInitAppSetsCorrectUIState : public AccTestStep<CalcTestContext> {
 public:
 
-    TestStep1InitApp()
+    TestStepInitAppSetsCorrectUIState(const std::string& name)
     : AccTestStep<CalcTestContext>(
-    "TestStep1InitApp", "When the app is initialized title bar, status bar, and contents must be set correctly.", false, false) {
+    name, "When the app is initialized title bar, status bar, and contents must be set correctly.", false, false) {
     }
 
     void Act() override {
         auto ctx = GetTestContext();
+        ctx->App = std::make_shared<MyCalcApplication>(ctx->UI);
         ctx->App->StartUp();
     }
 
@@ -178,222 +179,81 @@ public:
     }
 };
 
-// Another test step
-
-class TestStep2Add10ToResult : public AccTestStep<CalcTestContext> {
+// A parameterized test step: the parameters are passed in through constructor and determined when adding the instance to the 
+// scenario. 
+class TestStepInput_PressAdd_Status_Result : public AccTestStep<CalcTestContext> {
 public:
 
-    TestStep2Add10ToResult()
-    : AccTestStep<CalcTestContext>("TestStep2Adding10ToResult",
-    "When the number 10 is put in and Add button pressed, status bar must show Ready, and result must show 10.", false, false) {
+    TestStepInput_PressAdd_Status_Result(const std::string& testName,
+            const std::string& input, const std::string& status, const std::string& result)
+    : AccTestStep<CalcTestContext>(testName, CreateDescription(input, status, result), false, false),
+    m_Input(input), m_Status(status), m_Result(result) {
     }
 
     void Act() override {
         auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "10";
+        ui->m_TextBoxContents = m_Input;
         ui->m_AddButtonCallBack();
     }
 
     void Verify() override {
         auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "10";
+        bool success = ui->m_StatusBar == m_Status && ui->m_ResultContents == m_Result;
         SetPassed(success);
     }
+
+private:
+
+    static std::string CreateDescription(const std::string& input, const std::string& status, const std::string& result) {
+        std::ostringstream desc;
+        desc << "When the string \"" << input << "\" is put in and the Add button pressed, status bar must show \"" <<
+                status << "\" and the result must show \"" << result << "\".";
+        return desc.str();
+    }
+
+    std::string m_Input, m_Status, m_Result;
 };
 
 // Another test step
 
-class TestStep3Add20ToResult : public AccTestStep<CalcTestContext> {
+class TestStepInput_PressSubtract_Status_Result : public AccTestStep<CalcTestContext> {
 public:
 
-    TestStep3Add20ToResult()
-    : AccTestStep<CalcTestContext>("TestStep3Adding20ToResult",
-    "When the number 20 is put in and Add button pressed, status bar must show Ready, and result must show 30.", false, false) {
+    TestStepInput_PressSubtract_Status_Result(const std::string& testName,
+            const std::string& input, const std::string& status, const std::string& result)
+    : AccTestStep<CalcTestContext>(testName, CreateDescription(input, status, result), false, false),
+    m_Input(input), m_Status(status), m_Result(result) {
     }
 
     void Act() override {
         auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "20";
-        ui->m_AddButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "30";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep4Subtract15MustYield15 : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep4Subtract15MustYield15()
-    : AccTestStep<CalcTestContext>("TestStep4Subtract15MustYield15",
-    "When the number 15 is put in and Subtract button pressed, status bar must show Ready, and result must show 15.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "15";
+        ui->m_TextBoxContents = m_Input;
         ui->m_SubtractButtonCallBack();
     }
 
     void Verify() override {
         auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "15";
+        bool success = ui->m_StatusBar == m_Status && ui->m_ResultContents == m_Result;
         SetPassed(success);
     }
+
+    static std::string CreateDescription(const std::string& input, const std::string& status, const std::string& result) {
+        std::ostringstream desc;
+        desc << "When the string \"" << input << "\" is put in and the Subtract button pressed, status bar must show \"" <<
+                status << "\" and the result must show \"" << result << "\".";
+        return desc.str();
+    }
+
+    std::string m_Input, m_Status, m_Result;
 };
 
 // Another test step
 
-class TestStep5Subtract7MustYield8 : public AccTestStep<CalcTestContext> {
+class TestStepExitingAppMustCloseTheUI : public AccTestStep<CalcTestContext> {
 public:
 
-    TestStep5Subtract7MustYield8()
-    : AccTestStep<CalcTestContext>("TestStep5Subtract7MustYield8",
-    "When the number 7 is put in and Subtract button pressed, status bar must show Ready, and result must show 8.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "7";
-        ui->m_SubtractButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "8";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep6Add52MustYield60 : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep6Add52MustYield60()
-    : AccTestStep<CalcTestContext>("TestStep6Add52MustYield60",
-    "When the number 52 is put in and Add button pressed, status bar must show Ready, and result must show 60.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "52";
-        ui->m_AddButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "60";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep7AddNonNumericMustGetError : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep7AddNonNumericMustGetError()
-    : AccTestStep<CalcTestContext>("TestStep7AddNonNumericMustGetError",
-    "When a non-numerical string is put in and Add button pressed, status bar must show Error, and result must still be showing 60.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "sn3wfsf";
-        ui->m_AddButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Error" && ui->m_ResultContents == "60";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep8Subtract23MustShow37 : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep8Subtract23MustShow37()
-    : AccTestStep<CalcTestContext>("TestStep8Subtract23MustShow37",
-    "When a when 23 is put in and Subtract button pressed, status bar must show Ready, and result must show 37.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "23";
-        ui->m_SubtractButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "37";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep9SubtractNonNumericMustGetError : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep9SubtractNonNumericMustGetError()
-    : AccTestStep<CalcTestContext>("TestStep9SubtractNonNumericMustGetError",
-    "When a non-numerical string is put in and Subtract button pressed, "
-    "status bar must show Error, and result must still be showing 37.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "3wfsf";
-        ui->m_SubtractButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Error" && ui->m_ResultContents == "37";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep10Add32MustShow69 : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep10Add32MustShow69()
-    : AccTestStep<CalcTestContext>("TestStep10Add32MustShow69",
-    "When the number 32 is put in and Add button pressed, status bar must show Ready, and result must show 69.", false, false) {
-    }
-
-    void Act() override {
-        auto ui = GetTestContext()->UI;
-        ui->m_TextBoxContents = "32";
-        ui->m_AddButtonCallBack();
-    }
-
-    void Verify() override {
-        auto ui = GetTestContext()->UI;
-        bool success = ui->m_StatusBar == "Ready" && ui->m_ResultContents == "69";
-        SetPassed(success);
-    }
-};
-
-// Another test step
-
-class TestStep11WhenExitingAppTheUIMustBeClosed : public AccTestStep<CalcTestContext> {
-public:
-
-    TestStep11WhenExitingAppTheUIMustBeClosed()
-    : AccTestStep<CalcTestContext>("TestStep11WhenExitingAppTheUIMustBeClosed",
-    "When close request is sent to the app, "
+    TestStepExitingAppMustCloseTheUI(const std::string& name)
+    : AccTestStep<CalcTestContext>(name, "When close request is sent to the app, "
     "the Close() from user interface must be called once.", false, false) {
     }
 
@@ -403,8 +263,8 @@ public:
     }
 
     void Act() override {
-        auto app = GetTestContext()->App;
-        app->Exit();
+        GetTestContext()->App->Exit();
+        GetTestContext()->App = nullptr;
     }
 
     void Verify() override {
@@ -423,28 +283,45 @@ class MyTestScenario : public AccTestScenario<CalcTestContext> {
 public:
 
     MyTestScenario() {
-        AddStep(std::make_shared<TestStep1InitApp>());
-        AddStep(std::make_shared<TestStep2Add10ToResult>());
-        AddStep(std::make_shared<TestStep3Add20ToResult>());
-        AddStep(std::make_shared<TestStep4Subtract15MustYield15>());
-        AddStep(std::make_shared<TestStep5Subtract7MustYield8>());
-        AddStep(std::make_shared<TestStep6Add52MustYield60>());
-        AddStep(std::make_shared<TestStep7AddNonNumericMustGetError>());
-        AddStep(std::make_shared<TestStep8Subtract23MustShow37>());
-        AddStep(std::make_shared<TestStep9SubtractNonNumericMustGetError>());
-        AddStep(std::make_shared<TestStep10Add32MustShow69>());
-        AddStep(std::make_shared<TestStep11WhenExitingAppTheUIMustBeClosed>());
+        AddStep(std::make_shared<TestStepInitAppSetsCorrectUIState>("1_InitApp_MustSet_Status_Result_Title"));
+        AddStep(std::make_shared<TestStepInput_PressAdd_Status_Result>(
+                "2_Input10_Add_Result10_Status_Ready", "10", "Ready", "10"));
+        AddStep(std::make_shared<TestStepInput_PressAdd_Status_Result>(
+                "3_Input20_Add_Result30_Status_Ready", "20", "Ready", "30"));
+        AddStep(std::make_shared<TestStepInput_PressSubtract_Status_Result>(
+                "4_Input15_Subtract_Result15_Status_Ready", "15", "Ready", "15"));
+        AddStep(std::make_shared<TestStepInput_PressSubtract_Status_Result>(
+                "5_Input7_Subtract_Result8_Status_Ready", "7", "Ready", "8"));
+        AddStep(std::make_shared<TestStepInput_PressAdd_Status_Result>(
+                "6_Input52_Add_Result60_Status_Ready", "52", "Ready", "60"));
+        AddStep(std::make_shared<TestStepInput_PressAdd_Status_Result>(
+                "7_Input\"3gsg\"_Add_Result60_Status_Error", "3gsg", "Error", "60"));
+        AddStep(std::make_shared<TestStepInput_PressSubtract_Status_Result>(
+                "8_Input23_Subtract_Result37_Status_Ready", "23", "Ready", "37"));
+        AddStep(std::make_shared<TestStepInput_PressSubtract_Status_Result>(
+                "9_Input\"sdvn8e\"_Subtract_Result8_Status_Ready", "sdvn8e", "Error", "37"));
+        AddStep(std::make_shared<TestStepInput_PressAdd_Status_Result>(
+                "10_Input32_Add_Result69_Status_Ready", "32", "Ready", "69"));
+        AddStep(std::make_shared<TestStepExitingAppMustCloseTheUI>("11_ExitApp_UIMustBeClosed"));
+        AddStep(std::make_shared<TestStepInitAppSetsCorrectUIState>("12_InitApp_MustSet_Status_Result_Title"));
+        // Yes, we can start the app again after exiting! Sure some scenarios work like that; like when you're testing persistence 
+        // of app state, you definitely need to exit the app and start it again to see if things stay the same after restarting.
+        AddStep(std::make_shared<TestStepInput_PressSubtract_Status_Result>(
+                "13_Input-25_Subtract_Result25_Status_Ready", "-25", "Ready", "25"));
+        // Step 13 will fail because our calculator doesn't accommodate for negative numbers and treats them as non-numerical
+        // strings. Therefore, our subsequent steps that count on current app state will probably fail.
+        AddStep(std::make_shared<TestStepInput_PressAdd_Status_Result>(
+                "14_Input20_Add_Result45_Status_Ready", "20", "Ready", "45"));
+        AddStep(std::make_shared<TestStepExitingAppMustCloseTheUI>("15_ExitApp_UIMustBeClosed"));
     }
 
     void Setup() override {
         auto ctx = GetTestContext();
         ctx->UI = new FakeCaclUserInterface();
-        ctx->App = new MyCalcApplication(ctx->UI);
     }
 
     void Teardown() override {
         auto ctx = GetTestContext();
-        delete ctx->App;
         delete ctx->UI;
     }
 
